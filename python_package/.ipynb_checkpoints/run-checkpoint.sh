1b105be9d0c0898062c5883e6f1531ef4e5b1151
Shintaro Masuno <shintaromasuno@gmail.com>
2026-03-22 10:42:07 -0700

    initial commit

---
+++ python_package/.ipynb_checkpoints/run-checkpoint.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+VOCAB_FILE='vocab_21k.json'
+VOCAB_SIZE=''
+
+ARG_COMPILE=--${2:-'no-compile'}
+ARG_BACKEND=--backend=${3:-'inductor'}
+
+# Download data to container
+python download_data.py
+
+# Test CPU version
+#python run.py train \
+#    --train-src=./data/train.ja \
+#    --train-tgt=./data/train.en \
+#    --dev-src=./data/dev.ja \
+#    --dev-tgt=./data/dev.en \
+#    --vocab=vocab_21k.json \
+#    --vocab-size-src=21000 \
+#    --src-vocab-model="src_21k"
+#    $ARG_COMPILE 
+
+# GPU Version
+
+CUDA_VISIBLE_DEVICES=0 python run.py train \
+    --train-src=/tmp/data/train.ja \
+    --train-tgt=/tmp/data/train.en \
+    --dev-src=/tmp/data/dev.ja \
+    --dev-tgt=/tmp/data/dev.en \
+    --vocab=vocab_50k.json \
+    --vocab-size-src=50000 \
+    --src-vocab-model="src_50k" \
+    $ARG_COMPILE $ARG_BACKEND \
+    --gpu --lr=5e-4 --patience=1 \
+    --valid-niter=200 --batch-size=32 --dropout=.3
---
+++ python_package/.ipynb_checkpoints/setup-checkpoint.py
@@ -0,0 +1,24 @@
+
+from setuptools import find_packages
+from setuptools import setup
+import setuptools
+from distutils.command.build import build as _build
+import subprocess
+
+REQUIRED_PACKAGES = [
+    'sacrebleu',
+    'tqdm',
+    'docopt',
+    'sentencepiece',
+    'cloudml-hypertune',
+    'nltk'
+]
+
+setup(
+    name='trainer',
+    version='0.1',
+    install_requires=REQUIRED_PACKAGES,
+    packages=find_packages(),
+    include_package_data=True,
+    description='Vertex AI | Training | PyTorch | Neural Machine Translation with RNN | Python Package'
+)
